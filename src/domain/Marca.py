ac0ecb170b00ab7c3c6c01815aa6da1a73c260d4
Victor Hugo <victorsilva698@usp.br>
2021-04-11 18:16:32 -0300

    add : crated file for the class Marca

---
+++ src/domain/Marca.py
@@ -0,0 +1,17 @@
+
+
+tipoMarca: str[] = ['figurativa', 'nominativa', 'mista', 'tridimensionais']
+
+
+class IMarca:
+    nome: str
+    tipoMarca: str
+    autor : str
+    features
+
+class Marca():
+    def __init__(self,IMarca):
+        self.props = IMarca
+    
+    def create(self,IMarca):
+        
