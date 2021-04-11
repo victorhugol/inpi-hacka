dd47b169d0d357d5be6253c334eee1e994d35e4f
Victor Hugo <victorsilva698@usp.br>
2021-04-11 15:02:16 -0300

    added : added lines of code that add infos to .env

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import sys
 from src.index import run
+from os import environ
+with open('./.env') as env:
+  for line in env:
+    keyValueList = line.split('=')
+    if(len(keyValueList)>1):
+      environ[keyValueList[0]] = keyValueList[1]
+
 
 if __name__ == "__main__":
     if(sys.argv[1] == 'run'):
